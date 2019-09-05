c06cf55c3ab24bac8a0663d6a1e21056dc977fff
Oleh Moskaliuk <web.moskaliuk@gmail.com>
2019-09-05 12:57:58 +0300

    hello-world

--- src/xyz.java
+++ src/xyz.java
@@ -6,25 +6,24 @@ import java.time.LocalDateTime;
 public class xyz extends Application {
 
     public static void main(String[] args) {
-        byte cnuParts = 17;
-        short maxMark = 100;
-        int stepsToUniversity = 1443;
-        long indiaDemographic = 1283370000L;
-        float distanceBetweenCampus = 4.9F;
-        double squareUniversity = 2.8512D;
-        char exitProgram = 'Q';
+        byte keyboardKeys = 64;
+        System.out.println("Keyboard that I use has " + keyboardKeys + " keys");
+        short stepsToUniversity = 1443;
+        System.out.println("I have to make " + stepsToUniversity + " steps to get the 8 campus");
+        int screenPixels = 4096000;
+        System.out.println("My screen has " + screenPixels + " pixels");
+        long bigger = 999999999;
+        System.out.println("Some big number that I could input to 'long' variable is " + bigger);
+        float squareUniversity = 2.8512F;
+        System.out.println("Square of the university were I'm studying is " +  squareUniversity + " hectare");
+        double yoyoSquare = Math.PI * Math.pow(27.4, 2);
+        System.out.println("Square of circle of my yoyo is " + yoyoSquare);
+        char nameBegins = 'O';
+        System.out.println("My name begins form " + nameBegins + " letter");
         boolean logic = true;
+        System.out.println("I've been in Romania this summer, it is " + logic);
         LocalDateTime CompileDate = LocalDateTime.now();
-        System.out.println("Hello, World!");
-        System.out.println("This is the first laboratory work, there are variables with different data types examples");
-        System.out.println("Maximal mark is " + maxMark + " points");
-        System.out.println("I have to make " + stepsToUniversity + "steps to get the 8 campus");
-        System.out.println(squareUniversity + " hectare is square of CNU 8 and 9 campuses. CNU has " + cnuParts + " of it");
-        System.out.println("Distance between 8 campus and Residence of Bukovinian and Dalmatian Metropolitans is " + distanceBetweenCampus);
-        System.out.println("2 x 2 = 4 equal to " + logic);
-        System.out.println("India demographic is " + indiaDemographic);
-        System.out.println("Press button combination 'command' + '" + exitProgram + "' to exit current program");
-        System.out.println("Date of program compiling is: " + CompileDate);
+        System.out.println(CompileDate);
     }
 
     @Override
